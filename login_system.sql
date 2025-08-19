2229a0ec3aa30c9458fddee46a53f16c04ff8ee1
lustriaa <lustria.marienella@gmail.com>
2025-08-20 00:14:38 +0800

    Add files via upload

---
+++ login_system.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 19, 2025 at 03:35 PM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_system`
+--
+
+DROP TABLE IF EXISTS `login_system`;
+CREATE TABLE IF NOT EXISTS `login_system` (
+  `username` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `login_system`
+--
+
+INSERT INTO `login_system` (`username`, `mobile`, `password`) VALUES
+('ella_lustria', '09426402380', '@budo0306'),
+('amir_arabia', '09123456789', '@amir0306'),
+('lily_martinez', '09987654321', '@lily0306');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
